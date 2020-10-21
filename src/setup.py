3b2ce547c668afbaddcc99f61f1b12d78be1a454
Aurexine <Aurexine@gmail.com>
2020-10-20 19:17:58 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,28 +1,26 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-from cutesnowflakes.cutesnowflakes import VERSION
+with open("../README.md") as fp:
+    long_description = fp.read()
 
 setup(
     name="cutesnowflakes",
-    version=VERSION,
+    version="0.2.0",
     description="Cute 3x3 images from snowflake IDs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/CodeBizarre/cutesnowflakes",
     author="CodeBizarre",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
         "Programming Language :: Python :: 3"
     ],
-    packages=find_packages(),
+    packages=["cutesnowflakes"],
     include_package_data=True,
+    python_requires=">=3.6",
     install_requires=[
         "numpy>=1.19<1.20",
         "pillow==8.0"
-    ],
-    entry_points={
-        "console_scripts": [
-            "cutesnowflakes=cutesnowflakes:main"
-        ]
-    }
+    ]
 )
