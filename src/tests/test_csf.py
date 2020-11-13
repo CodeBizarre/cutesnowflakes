22d55c2a644202c80385fde133363536654ad9f9
Aurexine <Aurexine@gmail.com>
2020-11-13 07:06:00 -0600

    Increase test coverage

--- src/tests/test_csf.py
+++ src/tests/test_csf.py
@@ -1,13 +1,16 @@
+import sys
 import os
 import pytest
 
 from PIL.PngImagePlugin import PngImageFile
 
-from cutesnowflakes.cutesnowflakes import CuteSnowflakes
+from cutesnowflakes.cutesnowflakes import CuteSnowflakes, print_usage
 
+uid_17 = "00000000000010001"
 uid_18 = "674438327927308358"
 uid_19 = "1315158838627233792"
 uid_20 = "01189998819991197253"
+uid_21 = "123123123123123123123"
 
 colors = ["grey", "red", "green", "blue", "purple", "magenta", "yellow", "orange"]
 
@@ -31,22 +34,51 @@ def encode(scope="function"):
 
     delete_test_png()
 
+def test_usage(capsys):
+    print_usage()
+
+    assert capsys.readouterr().out == \
+        f"Usage: {sys.argv[0]} <help | encode | decode\n" \
+        "encode <snowflake> [color] [r] [g] [b]\n" \
+        "decode <path/to/file.png>>\n"
+
 def test_decode():
     csf = CuteSnowflakes()
 
-    with PngImageFile(f"src/tests/{uid_18}.PNG") as fp:
+    with PngImageFile(f"src/tests/{uid_18}.png") as fp:
         result = csf.decode(fp)
 
     assert result == uid_18
 
+def test_too_large():
+    csf = CuteSnowflakes()
+
+    with pytest.raises(ValueError):
+        with PngImageFile("src/tests/enlarged.png") as fp:
+            csf.decode(fp)
+
+def test_no_metadata(capsys):
+    csf = CuteSnowflakes()
+
+    with PngImageFile("src/tests/no_meta.png") as fp:
+        csf.decode(fp)
+
+        assert capsys.readouterr().out == \
+            "Warning: Unable to fetch image metadata, using default value (Red).\n"
+
 @pytest.mark.parametrize(
     "uid,expected", [(uid_18, uid_18), (uid_19, uid_19), (uid_20, uid_20)]
 )
-def test_encode(encode, uid, expected):
+def test_encode_good(encode, uid, expected):
     result, uid_result = encode(uid)
 
     assert result.size == (3, 3)
     assert uid_result == expected
+
+@pytest.mark.parametrize("uid", [uid_17, uid_21])
+def test_encode_bad(encode, uid):
+    with pytest.raises(ValueError):
+        result, uid_result = encode(uid)
 
 @pytest.mark.parametrize("color", colors)
 def test_set_mode(color):
@@ -74,6 +106,9 @@ def test_custom_encode():
     with PngImageFile("test.png") as fp:
         assert csf.decode(fp) == uid_18
 
+    with pytest.raises(ValueError):
+        csf.set_custom((100, 150))
+
     if os.path.exists("test.png"): os.remove("test.png")
 
 def test_set_mode_error():
@@ -81,3 +116,7 @@ def test_set_mode_error():
 
     with pytest.raises(ValueError):
         csf.set_mode("incorrect color")
+
+def test_init_error():
+    with pytest.raises(ValueError):
+        CuteSnowflakes(mode="blurple")
